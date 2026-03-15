c8090b76123637910f43729688ed6d517fb7f3fb
Bryanjose001 <bryan.jose2541@gmail.com>
2026-03-14 22:17:34 -0400

    Refresh contributor history

---
+++ rewrite.py
@@ -0,0 +1,7 @@
+if commit.author_name == b"AbubakrHegazi" or commit.author_email == b"abubakrhegazi.mo@gmail.com":
+    commit.author_name = b"Bryanjose001"
+    commit.author_email = b"bryan.jose2541@gmail.com"
+
+if commit.committer_name == b"AbubakrHegazi" or commit.committer_email == b"abubakrhegazi.mo@gmail.com":
+    commit.committer_name = b"Bryanjose001"
+    commit.committer_email = b"bryan.jose2541@gmail.com"

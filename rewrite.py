2bbc804023eabd6a0911c0d444925797d7e83101
Bryanjose001 <bryan.jose2541@gmail.com>
2026-03-15 19:41:12 -0400

    fixing small bugs

--- rewrite.py
+++
@@ -1,7 +0,0 @@
-if commit.author_name == b"AbubakrHegazi" or commit.author_email == b"abubakrhegazi.mo@gmail.com":
-    commit.author_name = b"Bryanjose001"
-    commit.author_email = b"bryan.jose2541@gmail.com"
-
-if commit.committer_name == b"AbubakrHegazi" or commit.committer_email == b"abubakrhegazi.mo@gmail.com":
-    commit.committer_name = b"Bryanjose001"
-    commit.committer_email = b"bryan.jose2541@gmail.com"

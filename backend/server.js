2b3ab65b20866defb9a97f44d3d36d281f7a12b4
Bryan Camacho <bryanjose431@gmail.com>
2026-03-19 16:53:10 -0400

    fixin

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,11 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
-
 
 app.use(cors())
 app.use(express.json())
